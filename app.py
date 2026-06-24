68539456846c944ef2b37a079be46468ad751d70
sumayyasalam <sumayyava30@gmail.com>
2026-06-24 20:23:09 +0400

    Enhance app.py with book recommendation features

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 import streamlit as st
 import pickle
 import pandas as pd
-st.write("App Started")
 
+st.title("📚 Book Recommendation System")
+
+# Load Popular Books
 popular_df = pickle.load(open('popular.pkl','rb'))
 
 st.subheader("Top Recommended Books")
-
 st.dataframe(popular_df.head(10))
-st.write("Pivot Shape:", pt.shape)
+
+# Load Pivot File
+pt = pickle.load(open('pivot.pkl','rb'))
+
+st.write("Pivot Shape:")
+st.write(pt.shape)
+
+# Load Similarity File
+similarity_scores = pickle.load(open('similarity.pkl','rb'))
 
 st.write("Similarity Shape:")
 st.write(similarity_scores.shape)
-pt = pickle.load(open('pivot.pkl','rb'))
-
-similarity_scores = pickle.load(open('similarity.pkl','rb'))
