b1e8becd4b130859f3bee18c6e8a1d1be21bbd63
sumayyasalam <sumayyava30@gmail.com>
2026-06-24 20:25:31 +0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,49 @@ st.write("Pivot Shape:")
 st.write(pt.shape)
 
 # Load Similarity File
-similarity_scores = pickle.load(open('similarity.pkl','rb'))
+st.header("📚 Get Book Recommendations")
 
-st.write("Similarity Shape:")
-st.write(similarity_scores.shape)
+selected_book = st.selectbox(
+    "Choose a Book",
+    pt.index.values
+)
+
+def recommend(book_name):
+
+    index = np.where(
+        pt.index == book_name
+    )[0][0]
+
+    similar_items = sorted(
+        list(
+            enumerate(
+                similarity_scores[index]
+            )
+        ),
+        key=lambda x: x[1],
+        reverse=True
+    )[1:6]
+
+    recommendations = []
+
+    for i in similar_items:
+        recommendations.append(
+            pt.index[i[0]]
+        )
+
+    return recommendations
+
+if st.button("Recommend"):
+
+    recommendations = recommend(
+        selected_book
+    )
+
+    st.subheader(
+        "Recommended Books"
+    )
+
+    for book in recommendations:
+        st.write("📖", book)
+
+import numpy as np
